0f772f1f976553fcf812b034da85054835522a65
SpacioRift <107812609+SpacioRift@users.noreply.github.com>
2022-10-04 21:56:55 +0200

    Update TD - 2.c

--- TD - 2.c
+++ TD - 2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 100;
+typedef char tabEmploye[MAX];
+typedef char Chaine[15];
+
 typedef struct{
     char nom[15], prenom[15];
     int code;
@@ -26,9 +30,23 @@ void afficherEmploye(Employe * e){
     printf("Salaire : %f \n", (*e).salaire);
 };
 
+void modifEmploye(Employe *e)
+{
+    printf("Entrez le nouveau nom de l'employe :\n ");
+    scanf("%s", (*e).nom);
+    printf("Entrez le nouveau prenom de l'employe :\n ");
+    scanf("%s", (*e).prenom);
+    printf("Entrez le nouveau code de fonction de l'employe :\n ");
+    scanf("%d", (*e).code);
+    printf("Entrez le nouveau salaire de l'employe :\n ");
+    scanf("%f", (*e).salaire);
+};
+
 int main(){
     Employe e;
     saisieEmploye(&e);
+    afficherEmploye(&e);
+    modifEmploye(&e);
     afficherEmploye(&e);
 
     return 0;
