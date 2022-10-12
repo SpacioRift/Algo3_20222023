59d5d088a5a24b80432dbb4886628df6f3796583
SpacioRift <107812609+SpacioRift@users.noreply.github.com>
2022-10-12 13:41:53 +0200

    Update TD - 3.c

--- TD - 3.c
+++ TD - 3.c
@@ -1,25 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void longueur(char chaine){
-    char *p;
-    p = chaine;
+void longueur(char* chaine){
+    printf("%s", chaine);
     int i = 0;
-    while(*p != '\0'){
-        printf("%c \n", *p);
-        i = i + 1;
-        p++;
+    while(chaine[i] != '\0'){
+        i++;
     };
     printf("longueur de la chaine : %d \n", i);
 };
 
-void afficheInverse(){
-
+void afficheInverse(char* chaine){
+    int i = longueur(chaine) -1;
+    while (i>=0){
+        printf("%c", chaine[i]);
+        i--;
+    }
 };
 
-int main(int argc, char **argv){
-    char * chaine = "";
-    printf("Entrez votre prénom :");
-        scanf("%s", &chaine);
+int equalChain(char* chaine, char* otherChain){
+    int i=0;
+    while(chaine[i] != '\0' && otherChain[i] == '\0'){
+        if(chaine[i] != otherChain[i]){
+            return 0;
+        }
+        i++
+    }
+
+    if(chaine[i] == '\0' && otherChain[i] == '\0'){
+        return 1;
+    }ele{
+        return 0;
+    }
+};
+
+int main(){
+    char chaine[25];
+    char otherChain[25];
+    printf("Entrez votre prénom :\n")
+        scanf("%c", &chaine);
+    printf("Entrez un autre prénom :\n");
+        scanf("%c", &otherChain);
     longueur(chaine);
+    longueur(otherChain);
+    afficheInverse(chaine);
+    afficheInverse(otherChain);
+    printf("\n");
+
+    if(equalChain(chaine, otherChain) == 1){
+    printf("Les deux chaines sont égales \n");
+    }else{
+        printf("Les deux chaines ne sont pas égales \n")
+    }
+    return 0;
 };
