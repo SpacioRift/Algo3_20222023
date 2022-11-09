c34614e1004a547bf02773605988a66953e88bc8
SpacioRift <107812609+SpacioRift@users.noreply.github.com>
2022-11-09 14:58:17 +0100

    Add files via upload

---
+++ exo 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef char Chaine[15];
+
+typedef struct _Client{
+    unsigned short idClient;
+    Chaine nomClient;
+}Client;
+
+typedef Client Tab[N];
+
+void saisie(Client *c){
+    printf("\nEntrer l'id du client : ");
+    scanf("%hd", &c->idClient);
+    printf("Entrer le nom du client : ");
+    scanf("%s", c->nomClient);
+}
